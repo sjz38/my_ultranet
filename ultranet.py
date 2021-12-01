d7ab195139998bae1079fb6c85c91a3d66e80407
Hyun Jong Lee <hl693@cornell.edu>
2021-12-01 11:44:54 -0500

    iou score

--- ultranet.py
+++ ultranet.py
@@ -492,29 +492,33 @@ def bbox_iou(box1, box2):
     """
     Returns the IoU of two bounding boxes
     """
+    box1 = torch.FloatTensor(box1)
+    box2 = torch.FloatTensor(box2)
  
     # Transform from center and width to exact coordinates
-    b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
-    b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
-    b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
-    b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+    b1_x1, b1_x2 = box1[0] - box1[2] / 2, box1[0] + box1[2] / 2
+    b1_y1, b1_y2 = box1[1] - box1[3] / 2, box1[1] + box1[3] / 2
+    b2_x1, b2_x2 = box2[0] - box2[2] / 2, box2[0] + box2[2] / 2
+    b2_y1, b2_y2 = box2[1] - box2[3] / 2, box2[1] + box2[3] / 2
     
     # get the corrdinates of the intersection rectangle
     inter_rect_x1 = torch.max(b1_x1, b2_x1)
     inter_rect_y1 = torch.max(b1_y1, b2_y1)
     inter_rect_x2 = torch.min(b1_x2, b2_x2)
     inter_rect_y2 = torch.min(b1_y2, b2_y2)
+
     # Intersection area
     inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1, min=0) * torch.clamp(
         inter_rect_y2 - inter_rect_y1, min=0
     )
+
     # Union Area
     b1_area = (b1_x2 - b1_x1) * (b1_y2 - b1_y1)
     b2_area = (b2_x2 - b2_x1) * (b2_y2 - b2_y1)
 
     iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
 
-    return iou
+    return iou.item()
 
-iou = bbox_iou(truth_result, result)
+iou = round(bbox_iou(truth_result, result), 4)
 print("iou score: ", iou)
