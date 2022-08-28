af61237ef57e00ad064c87eaed211351cb3d223f
sjz38 <sjz38@cornell.edu>
2022-08-28 15:34:48 -0400

    Move yolo conv into unet model; added bias to conv

--- main_single_input.py
+++ main_single_input.py
@@ -12,13 +12,15 @@ from ultranet_model import ultranet
 ###############################################################################
 # Define Data Types
 ###############################################################################
-hcl.init(hcl.Float(32))
+# hcl.init(hcl.Float(32))
+hcl.init(hcl.Fixed(16,8))
 input_dtype = hcl.Fixed(8, 4)
 weight_dtype = hcl.Fixed(5, 3) # TODO: why hcl.Fixed(4,4) doesn't work
 act_dtype = hcl.UFixed(6, 4)
 bn_a_dtype = hcl.Fixed(14, 10) # TODO some 14 bit fixed pt, this seems to work well 
 bn_b_dtype = hcl.Fixed(26, 18) # TODO some 26 bit fixed pt, this seems to work well
 conv_dtype = hcl.Fixed(16, 8)
+yolo_bias_dtype = hcl.Fixed(11,8) # TODO seems to work given values in bias term
 
 ###############################################################################
 # Define parameters and images
@@ -27,6 +29,8 @@ conv_dtype = hcl.Fixed(16, 8)
 image_path = './test_images/boat1_000001.jpg'
 # image_path = './test_images/person23_0113.jpg'
 # image_path = './test_images/car1_0001.jpg'
+print(image_path.split("/")[-1] + "\n")
+
 raw_height = 360
 raw_width = 640
 width = 320
@@ -100,6 +104,10 @@ def load_np_params(ptname):
     batchnorm8_running_mean = model['layers.26.running_mean'].numpy()
     batchnorm8_running_var = model['layers.26.running_var'].numpy()
 
+    yolo_weight = model['layers.28.weight'].numpy()
+    yolo_weight = weight_quantizer(yolo_weight)
+    yolo_bias = model['layers.28.bias'].numpy()
+
     print("Weights loaded from " + ptname + "\n")
 
     return [
@@ -110,10 +118,11 @@ def load_np_params(ptname):
         conv5_weight, batchnorm5_weight, batchnorm5_bias, batchnorm5_running_mean, batchnorm5_running_var,
         conv6_weight, batchnorm6_weight, batchnorm6_bias, batchnorm6_running_mean, batchnorm6_running_var,
         conv7_weight, batchnorm7_weight, batchnorm7_bias, batchnorm7_running_mean, batchnorm7_running_var,
-        conv8_weight, batchnorm8_weight, batchnorm8_bias, batchnorm8_running_mean, batchnorm8_running_var
+        conv8_weight, batchnorm8_weight, batchnorm8_bias, batchnorm8_running_mean, batchnorm8_running_var,
+        yolo_weight, yolo_bias
     ]
         
- # loads and resizes a single image
+# loads and resizes a single image
 def load_image(image_path):
     image = cv2.imread(str(image_path))
     image = cv2.resize(image, (width, height), interpolation=cv2.INTER_LINEAR)
@@ -163,6 +172,9 @@ def build_ultranet_inf(batch_size=batch_size, target=None):
     a_batchnorm8 = hcl.placeholder((64,), dtype=bn_a_dtype, name="a_batchnorm8")
     b_batchnorm8 = hcl.placeholder((64,), dtype=bn_b_dtype, name="b_batchnorm8")
 
+    weight_yolo = hcl.placeholder((36, 64, 1, 1), dtype=weight_dtype, name="weight_yolo") # 64 in, 36 out
+    bias_yolo = hcl.placeholder((36,), dtype=yolo_bias_dtype, name="bias_yolo")
+    
     sm = hcl.create_scheme(
         [input_image, 
         weight_conv1, a_batchnorm1, b_batchnorm1, 
@@ -172,7 +184,8 @@ def build_ultranet_inf(batch_size=batch_size, target=None):
         weight_conv5, a_batchnorm5, b_batchnorm5, 
         weight_conv6, a_batchnorm6, b_batchnorm6, 
         weight_conv7, a_batchnorm7, b_batchnorm7, 
-        weight_conv8, a_batchnorm8, b_batchnorm8], 
+        weight_conv8, a_batchnorm8, b_batchnorm8,
+        weight_yolo, bias_yolo], 
         ultranet
     )
 
@@ -193,6 +206,7 @@ def build_ultranet_inf(batch_size=batch_size, target=None):
     sm.quantize(ultranet.relu7, act_dtype)
     sm.quantize(ultranet.conv8, conv_dtype)
     sm.quantize(ultranet.relu8, act_dtype)
+    sm.quantize(ultranet.yolo, conv_dtype)
     
     s = hcl.create_schedule_from_scheme(sm, "main")
     return hcl.build(s, target=target)
@@ -246,6 +260,8 @@ if __name__ == "__main__":
     batchnorm8_bias = params[37]
     batchnorm8_running_mean = params[38]
     batchnorm8_running_var = params[39]
+    yolo_weight = params[40]
+    yolo_bias = params[41]
 
     ###############################################################################
     # Precompute a and b terms on CPU side for batchnorm ax+b
@@ -253,43 +269,27 @@ if __name__ == "__main__":
     epsilon = 10**-7
     batchnorm1_a = batchnorm1_weight / np.sqrt(batchnorm1_running_var + epsilon)
     batchnorm1_b = ((-1*batchnorm1_weight * batchnorm1_running_mean) / np.sqrt(batchnorm1_running_var + epsilon)) + batchnorm1_bias
-    # batchnorm1_a = weight_quantizer(batchnorm1_a)
-    # batchnorm1_b = weight_quantizer(batchnorm1_b)
 
     batchnorm2_a = batchnorm2_weight / np.sqrt(batchnorm2_running_var + epsilon)
     batchnorm2_b = ((-1*batchnorm2_weight * batchnorm2_running_mean) / np.sqrt(batchnorm2_running_var + epsilon)) + batchnorm2_bias
-    # batchnorm2_a = weight_quantizer(batchnorm2_a)
-    # batchnorm2_b = weight_quantizer(batchnorm2_b)
 
     batchnorm3_a = batchnorm3_weight / np.sqrt(batchnorm3_running_var + epsilon)
     batchnorm3_b = ((-1*batchnorm3_weight * batchnorm3_running_mean) / np.sqrt(batchnorm3_running_var + epsilon)) + batchnorm3_bias
-    # batchnorm3_a = weight_quantizer(batchnorm3_a)
-    # batchnorm3_b = weight_quantizer(batchnorm3_b)
 
     batchnorm4_a = batchnorm4_weight / np.sqrt(batchnorm4_running_var + epsilon)
     batchnorm4_b = ((-1*batchnorm4_weight * batchnorm4_running_mean) / np.sqrt(batchnorm4_running_var + epsilon)) + batchnorm4_bias
-    # batchnorm4_a = weight_quantizer(batchnorm4_a)
-    # batchnorm4_b = weight_quantizer(batchnorm4_b)
 
     batchnorm5_a = batchnorm5_weight / np.sqrt(batchnorm5_running_var + epsilon)
     batchnorm5_b = ((-1*batchnorm5_weight * batchnorm5_running_mean) / np.sqrt(batchnorm5_running_var + epsilon)) + batchnorm5_bias
-    # batchnorm5_a = weight_quantizer(batchnorm5_a)
-    # batchnorm5_b = weight_quantizer(batchnorm5_b)
 
     batchnorm6_a = batchnorm6_weight / np.sqrt(batchnorm6_running_var + epsilon)
     batchnorm6_b = ((-1*batchnorm6_weight * batchnorm6_running_mean) / np.sqrt(batchnorm6_running_var + epsilon)) + batchnorm6_bias
-    # batchnorm6_a = weight_quantizer(batchnorm6_a)
-    # batchnorm6_b = weight_quantizer(batchnorm6_b)
 
     batchnorm7_a = batchnorm7_weight / np.sqrt(batchnorm7_running_var + epsilon)
     batchnorm7_b = ((-1*batchnorm7_weight * batchnorm7_running_mean) / np.sqrt(batchnorm7_running_var + epsilon)) + batchnorm7_bias
-    # batchnorm7_a = weight_quantizer(batchnorm7_a)
-    # batchnorm7_b = weight_quantizer(batchnorm7_b)
 
     batchnorm8_a = batchnorm8_weight / np.sqrt(batchnorm8_running_var + epsilon)
     batchnorm8_b = ((-1*batchnorm8_weight * batchnorm8_running_mean) / np.sqrt(batchnorm8_running_var + epsilon)) + batchnorm8_bias
-    # batchnorm8_a = weight_quantizer(batchnorm8_a)
-    # batchnorm8_b = weight_quantizer(batchnorm8_b)
 
     ###############################################################################
     # convert weights into hcl
@@ -326,7 +326,10 @@ if __name__ == "__main__":
     hcl_a_batchnorm8 = hcl.asarray(batchnorm8_a.astype(float), dtype=bn_a_dtype)
     hcl_b_batchnorm8 = hcl.asarray(batchnorm8_b.astype(float), dtype=bn_b_dtype)
 
-    hcl_out = hcl.asarray(np.zeros((batch_size, 64, 10, 20)), dtype=hcl.Fixed(16,8))
+    hcl_weight_yolo = hcl.asarray(yolo_weight.astype(float), dtype=weight_dtype)
+    hcl_bias_yolo = hcl.asarray(yolo_bias.astype(float), dtype=yolo_bias_dtype)
+
+    hcl_out = hcl.asarray(np.zeros((batch_size, 36, 10, 20)), dtype=hcl.Fixed(16,8))
 
     ###############################################################################
     # Inference
@@ -341,37 +344,20 @@ if __name__ == "__main__":
         hcl_weight_conv6, hcl_a_batchnorm6, hcl_b_batchnorm6, 
         hcl_weight_conv7, hcl_a_batchnorm7, hcl_b_batchnorm7, 
         hcl_weight_conv8, hcl_a_batchnorm8, hcl_b_batchnorm8,
+        hcl_weight_yolo, hcl_bias_yolo,
         hcl_out
     )
-
     ###############################################################################
-    # Results up to YOLO layer
+    # Model Output
     ###############################################################################
     np_input = hcl_input.asnumpy()
-    # print(hcl_out)
-    # hcl_out = hcl.compute(hcl_out.shape, lambda *x : hcl.cast(hcl.Float(32), hcl_out[x]), name='result', dtype=hcl.Float(32))
     np_out = hcl_out.asnumpy()
-    np_out = np.float32(np.abs(np_out))
-    # print(f"np_out: {np_out[-1]}")
-    print(f"np_out type: {np_out.dtype}")
-    # np_out = np.load('../ultra_net/model/torch_output.npy')
+    # np_out = np.float32(np.abs(np_out))
+    ultranet_out = torch.tensor(np_out)
 
     ###############################################################################
-    # YOLO Layer
+    # YOLO Layer (after conv)
     ###############################################################################
-
-    # TEMPORARY: last conv using pytorch
-    model = torch.load('ultranet_4w4a.pt', map_location='cpu')['model']
-    yolo_weight = model['layers.28.weight'].numpy()
-    yolo_weight = weight_quantizer(yolo_weight)
-    yolo_weight = torch.tensor(yolo_weight)
-    yolo_bias = model['layers.28.bias']
-
-    tensor_out = torch.tensor(np_out)
-    print(f"Tensor_out type: {tensor_out.dtype}")
-    ultranet_out = nn.functional.conv2d(tensor_out, yolo_weight, bias=yolo_bias, stride=1, padding=0)
-
-    # print("up to YOLO layer complete")
 
     def create_grids(self, img_size=416, ng=(13, 13), device='cpu', type=torch.float32):
         nx, ny = ng  # x and y grid size
@@ -443,7 +429,6 @@ if __name__ == "__main__":
     pre_box = pre_box[..., :4] * torch.Tensor([raw_width/width, raw_height/height, raw_width/width, raw_height/height]).to('cpu')
 
     result = list()
-
     for box in pre_box:
         xmin = box[0] - box[2] / 2
         xmax = box[0] + box[2] / 2
